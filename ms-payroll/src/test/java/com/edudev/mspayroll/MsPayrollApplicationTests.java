e9fdcc4b83bb5fd69fbe5058c3d176b3d75b075d
Eduardo José <duduxss3@gmail.com>
2022-05-08 20:22:28 -0300

    main
    	:tada: Creating new ms ms-payroll

---
+++ ms-payroll/src/test/java/com/edudev/mspayroll/MsPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.mspayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
