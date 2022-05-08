58a01a784442566597fd2813c52f35f6b8efa4ce
Eduardo José <duduxss3@gmail.com>
2022-05-08 17:30:38 -0300

    main
    	:tada: ms-worker created

---
+++ ms-worker/src/main/java/com/edudev/msworker/MsWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.msworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorkerApplication.class, args);
+	}
+
+}
