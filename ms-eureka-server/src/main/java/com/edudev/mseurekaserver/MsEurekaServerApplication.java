afb533e002ca0d19bab2e50fa0975a62552d798b
Eduardo José <duduxss3@gmail.com>
2022-05-10 20:04:05 -0300

    main
    	:rocket: New project ms-eureka-server

---
+++ ms-eureka-server/src/main/java/com/edudev/mseurekaserver/MsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.mseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaServerApplication.class, args);
+	}
+
+}
