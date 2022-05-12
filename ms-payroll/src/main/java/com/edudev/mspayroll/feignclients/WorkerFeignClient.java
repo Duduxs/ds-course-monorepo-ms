9ac6c68b2fe9cfb09c005e9f71c7cb87dc30ef6c
Eduardo José <duduxss3@gmail.com>
2022-05-11 21:00:36 -0300

    main
    	:rocket: Configuring feign client to obey the new context path

--- ms-payroll/src/main/java/com/edudev/mspayroll/feignclients/WorkerFeignClient.java
+++ ms-payroll/src/main/java/com/edudev/mspayroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "ms-worker", path = "/workers")
+@FeignClient(name = "ms-worker", path = "/ms-worker/workers")
 public interface WorkerFeignClient {
 
     @GetMapping("{id}")
--- ms-payroll/src/main/java/com/edudev/mspayroll/services/PaymentService.java
+++ ms-payroll/src/main/java/com/edudev/mspayroll/services/PaymentService.java
@@ -6,8 +6,6 @@ import com.edudev.mspayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class PaymentService {
 
