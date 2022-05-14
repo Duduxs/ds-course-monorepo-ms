5161d693e74ccf751126643e0731b0fc45a3a5a6
Eduardo José <duduxss3@gmail.com>
2022-05-14 20:11:44 -0300

    main
    	:rocket: project ms-config-server created

---
+++ ms-config-server/src/test/java/org/edudev/msconfigserver/MsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edudev.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
