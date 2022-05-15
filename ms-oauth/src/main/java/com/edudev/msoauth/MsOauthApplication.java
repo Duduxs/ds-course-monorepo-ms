c8933a6e86e2f6cb9732fce7936fdc1702bc8478
Eduardo José <duduxss3@gmail.com>
2022-05-15 16:52:48 -0300

    main
    	:rocket: Configuring ms-oauth project

--- ms-oauth/src/main/java/com/edudev/msoauth/MsOauthApplication.java
+++ ms-oauth/src/main/java/com/edudev/msoauth/MsOauthApplication.java
@@ -2,8 +2,10 @@ package com.edudev.msoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsOauthApplication {
 
 	public static void main(String[] args) {
