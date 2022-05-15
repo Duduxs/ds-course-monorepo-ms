84206f3cfef6ee03baadcfa149ac3cf4614ba25c
Eduardo José <duduxss3@gmail.com>
2022-05-15 16:50:28 -0300

    main
    	:rocket: Project ms-oauth created

---
+++ ms-oauth/src/main/java/com/edudev/msoauth/MsOauthApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.msoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauthApplication.class, args);
+	}
+
+}
---
+++ ms-oauth/src/test/java/com/edudev/msoauth/MsOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.msoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
