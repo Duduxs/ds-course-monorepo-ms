e4a20f1bd3227ff234d89233a050d5b88af01ab9
Eduardo José <duduxss3@gmail.com>
2022-05-15 15:57:50 -0300

    main
    	:rocket: Database seeding in ms-user

--- ms-user/src/main/java/com/edudev/msuser/MsUserApplication.java
+++ ms-user/src/main/java/com/edudev/msuser/MsUserApplication.java
@@ -1,15 +1,25 @@
 package com.edudev.msuser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MsUserApplication {
+public class MsUserApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+	}
 }
